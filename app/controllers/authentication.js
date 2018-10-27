39b9d93f76676a21e2c9d25e16492888828d77f1
chizotavictor <chizotavictor@gmail.com>
2018-10-27 23:39:51 +0000

    first commit

---
+++ app/controllers/authentication.js
@@ -0,0 +1,10 @@
+/*!
+ * Module dependencies.
+ */
+
+exports.index = function (req, res) {
+//   res.render('authentication/login', {
+//     title: 'System Login'
+//   });
+    return res.send('hello world');
+};
